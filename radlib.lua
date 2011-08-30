a300882868c81caa0f55b1848bd5f4a20ed39f65
Radamanthus Batnag <rad@batnag.org>
2011-08-30 17:55:24 +0800

    Added table.print and io.parseJson to radlib

--- radlib.lua
+++ radlib.lua
@@ -2,6 +2,18 @@
 
 module(..., package.seeall)
 
+function io.parseJson( filename )
+  local file = io.open( filename, "r" )
+  if file then
+    local contents = file:read( "*a" )
+    result = json.decode( contents ) 
+    io.close( file )
+    return result
+  else
+    return {}
+  end
+end
+
 -- From: http://stackoverflow.com/questions/1283388/lua-merge-tables
 function table.merge(t1, t2)
   for k,v in pairs(t2) do
@@ -34,7 +46,14 @@ function table.filter( t, str )
   return result 
 end
 
+function table.print( t )
+  for i,v in ipairs(t) do
+    print(i .. " = " .. v)
+  end
+end
+
 function debug( msg )
   native.showAlert("DEBUG", msg, {"OK"})
 end
+
 
