700b64f0b724d4792d5cd63af07c4d245eec65bc
Prathamesh A Degwekar <prat.degwekar@gmail.com>
2017-07-04 15:07:39 +0530

    updated config.lua
    modernized config.lua file to fit all screen specs

--- config.lua
+++ config.lua
@@ -1,9 +1,10 @@
+local aspectRatio = display.pixelHeight / display.pixelWidth
 application =
 {
   content =
   {
-    width = 320,
-    height = 480,
+    width = aspectRatio > 1.5 and 320 or math.ceil( 480 / aspectRatio ),
+    height = aspectRatio < 1.5 and 480 or math.ceil( 320 * aspectRatio ),
     scale = "letterbox",
     imageSuffix =
     {
